46b3e001e81a938a9f64c3efdc73e406a2c5d93f
Favour Jam <jammoben@gmail.com>
2023-10-26 12:21:45 +0100

    added dialog boxes in the form section of the register page

--- src/components/FormCards.jsx
+++ src/components/FormCards.jsx
@@ -1,4 +1,7 @@
+"use client";
 import Button from "./Button.jsx";
+import { useEffect, useRef, useState } from "react";
+
 export function FormOne({ onClick }) {
   return (
     <>
@@ -21,28 +24,65 @@ export function FormOne({ onClick }) {
 
 export function FormTwo({ onClick, onClickPrev }) {
   const foodTypeInformation = [
-    "vegan",
-    "vegetarian",
-    "omnivore",
-    "paleo",
-    "pescatarian",
-    "carnivore",
-    "flexiterian",
-    "keto",
-    "gluten-free",
-    "lactose-free",
-    "dairy-free",
-    "shellfish-free",
-    "soy-free",
-    "allergen-free",
-    "low-carb",
-    "mediterranean",
-    "low-fat",
-    "low-sugar",
-    "low-food",
+    { type: "vegan", description: "No animal products." },
+    { type: "vegetarian", description: "Plant-based diet without meat." },
+    { type: "omnivore", description: "Eats both plants and animals." },
+    {
+      type: "paleo",
+      description: "Emphasizes whole foods, like our ancestors.",
+    },
+    { type: "pescatarian", description: "Vegetarian with fish and seafood." },
+    { type: "carnivore", description: "Primarily meat-based diet." },
+    {
+      type: "flexitarian",
+      description: "Mainly plant-based with occasional meat.",
+    },
+    { type: "keto", description: "High-fat, low-carb diet for ketosis." },
+    { type: "gluten-free", description: "Avoids gluten-containing foods." },
+    { type: "lactose-free", description: "Avoids lactose in dairy products." },
+    { type: "dairy-free", description: "Excludes all dairy products." },
+    {
+      type: "shellfish-free",
+      description: "Avoids shellfish due to allergies.",
+    },
+    { type: "soy-free", description: "Excludes soy-based foods." },
+    { type: "allergen-free", description: "Avoids common allergens." },
+    { type: "low-carb", description: "Restricts carbohydrates." },
+    { type: "mediterranean", description: "Based on Mediterranean cuisine." },
+    { type: "low-fat", description: "Emphasizes low-fat foods." },
+    { type: "low-sugar", description: "Limits sugar intake." },
+    { type: "low-food", description: "Reduces overall food consumption." },
   ];
+
+  const modal = useRef(null);
+  const [type, setType] = useState("");
+  const [description, setDescription] = useState("");
+  const handleModalclose = (event) => {
+    event.preventDefault();
+    modal.current.close();
+  };
+  const handleModalclick = (event, type, description) => {
+    event.preventDefault();
+    modal.current.showModal();
+    setType(type);
+    setDescription(description);
+  };
   return (
     <>
+      {/* dynamic dialog box */}
+      <dialog
+        ref={modal}
+        className="min-h-fit min-w-fit bg-white rounded-md relative"
+      >
+        <div className="p-2 grid place-items-center gap-1">
+          <h1 className="text-center text-lg font-bold">{type}</h1>
+          <p className="text-center w-3/4">{description}</p>
+          <button onClick={handleModalclose} className="tile">
+            close
+          </button>
+        </div>
+      </dialog>
+      {/*  */}
       <div className="flex flex-col justify-center items-left gap-3 min-w-full min-h-fit p-2 rounded-md flex-1">
         <h1 className="grid place-items-center text-secondary">
           Help with information about you
@@ -56,19 +96,32 @@ export function FormTwo({ onClick, onClickPrev }) {
           />
         </div>
         <div className="min-h-fitrounded-md flex flex-wrap justify-center items-center gap-2 w-full">
-          {foodTypeInformation.slice(0, 10).map((food, idx) => {
+          <button className="tile">
+            <img
+              src="/icons/add.png"
+              className="rotate-45"
+              alt="go full screen icon"
+              width={20}
+              height={20}
+            />
+            none
+          </button>
+          {foodTypeInformation.slice(0, 10)?.map((food, idx) => {
             return (
-              <>
-                <button className="tile" key={idx}>
+              <div key={idx}>
+                <div className="tile">
                   <img
+                    onClick={(e) =>
+                      handleModalclick(e, food?.type, food?.description)
+                    }
                     src="/icons/info.png"
                     alt="information icon"
                     width={20}
                     height={20}
                   />
-                  {food}
-                </button>
-              </>
+                  {food?.type}
+                </div>
+              </div>
             );
           })}
           <button className="tile">
@@ -92,21 +145,48 @@ export function FormTwo({ onClick, onClickPrev }) {
 
 export function FormThree({ onClick, onClickPrev }) {
   const Allergies = [
-    "Peanut",
-    "Tree Nut",
-    "Milk",
-    "Mustard",
-    "Fish",
-    "Egg",
-    "Soy",
-    "Wheat",
-    "Sesame",
-    "corn",
-    "shellfish",
-    "Meat",
+    { type: "Peanuts", description: "Allergic to peanuts." },
+    { type: "Tree Nuts", description: "Allergic to tree nuts." },
+    { type: "Milk", description: "Lactose or milk allergy." },
+    { type: "Mustard", description: "Allergic to mustard." },
+    { type: "Fish", description: "Allergic to fish." },
+    { type: "Egg", description: "Allergic to eggs." },
+    { type: "Soy", description: "Allergic to soy products." },
+    { type: "Wheat", description: "Wheat or gluten allergy." },
+    { type: "Sesame", description: "Allergic to sesame seeds." },
+    { type: "Corn", description: "Allergic to corn products." },
+    { type: "Shellfish", description: "Allergic to shellfish." },
+    { type: "Meat", description: "Allergic to meat products." },
   ];
+
+  const modal = useRef(null);
+  const [type, setType] = useState("");
+  const [description, setDescription] = useState("");
+  const handleModalclose = (event) => {
+    event.preventDefault();
+    modal.current.close();
+  };
+  const handleModalclick = (event, type, description) => {
+    event.preventDefault();
+    modal.current.showModal();
+    setType(type);
+    setDescription(description);
+  };
   return (
     <>
+      {/* dynamic dialog box */}
+      <dialog
+        ref={modal}
+        className="min-h-fit min-w-fit bg-white rounded-md relative"
+      >
+        <div className="p-2 grid place-items-center gap-1">
+          <h1 className="text-center text-lg font-bold">{type}</h1>
+          <p className="text-center w-3/4">{description}</p>
+          <button onClick={handleModalclose} className="tile">
+            close
+          </button>
+        </div>
+      </dialog>
       <div className="flex flex-col justify-center items-left gap-3 min-w-full min-h-fit p-2 rounded-md flex-1">
         <h1 className="grid place-items-center text-secondary">
           Any Allergies
@@ -120,21 +200,39 @@ export function FormThree({ onClick, onClickPrev }) {
           />
         </div>
         <div className="min-h-fitrounded-md flex flex-wrap justify-center items-center gap-2 w-full">
+          <button className="tile">
+            <img
+              src="/icons/add.png"
+              className="rotate-45"
+              alt="go full screen icon"
+              width={20}
+              height={20}
+            />
+            none
+          </button>
           {Allergies.slice(0, 10).map((allergy, idx) => {
             return (
-              <>
-                <button className="tile" key={idx}>
+              <div key={idx}>
+                <div className="tile">
                   <img
+                    onClick={(event) =>
+                      handleModalclick(
+                        event,
+                        allergy?.type,
+                        allergy?.description
+                      )
+                    }
                     src="/icons/info.png"
                     alt="information icon"
                     width={20}
                     height={20}
                   />
-                  {allergy}s
-                </button>
-              </>
+                  {allergy?.type}
+                </div>
+              </div>
             );
           })}
+
           <button className="tile">
             <img
               src="/icons/full-screen.png"
@@ -156,21 +254,56 @@ export function FormThree({ onClick, onClickPrev }) {
 
 export function FormFour({ onClick, onClickPrev }) {
   const chronicConditions = [
-    "Peanut",
-    "Tree Nut",
-    "Milk",
-    "Mustard",
-    "Fish",
-    "Egg",
-    "Soy",
-    "Wheat",
-    "Sesame",
-    "corn",
-    "shellfish",
-    "Meat",
+    { type: "Diabetes", description: "Affects blood sugar regulation." },
+    { type: "Hypertension", description: "High blood pressure." },
+    {
+      type: "Asthma",
+      description: "Respiratory condition causing breathing difficulties.",
+    },
+    { type: "Heart Disease", description: "Cardiovascular health issues." },
+    { type: "Arthritis", description: "Inflammation of the joints." },
+    { type: "Cancer", description: "Abnormal cell growth." },
+    {
+      type: "Alzheimer's",
+      description: "Progressive memory loss and cognitive decline.",
+    },
+    { type: "Chronic Pain", description: "Persistent, long-term pain." },
+    { type: "Osteoporosis", description: "Weakening of bones." },
+    { type: "Obesity", description: "Excessive body weight." },
+    {
+      type: "Chronic Kidney Disease",
+      description: "Kidney function impairment.",
+    },
+    { type: "COPD", description: "Chronic lung diseases like emphysema." },
   ];
+  const modal = useRef(null);
+  const [type, setType] = useState("");
+  const [description, setDescription] = useState("");
+  const handleModalclose = (event) => {
+    event.preventDefault();
+    modal.current.close();
+  };
+  const handleModalclick = (event, type, description) => {
+    event.preventDefault();
+    modal.current.showModal();
+    setType(type);
+    setDescription(description);
+  };
   return (
     <>
+      {/* dynamic dialog box */}
+      <dialog
+        ref={modal}
+        className="min-h-fit min-w-fit bg-white rounded-md relative"
+      >
+        <div className="p-2 grid place-items-center gap-1">
+          <h1 className="text-center text-lg font-bold">{type}</h1>
+          <p className="text-center w-3/4">{description}</p>
+          <button onClick={handleModalclose} className="tile">
+            close
+          </button>
+        </div>
+      </dialog>
       <div className="flex flex-col justify-center items-left gap-3 min-w-full min-h-fit p-2 rounded-md flex-1">
         <h1 className="grid place-items-center text-secondary">
           Chronic Conditions
@@ -184,19 +317,36 @@ export function FormFour({ onClick, onClickPrev }) {
           />
         </div>
         <div className="min-h-fitrounded-md flex flex-wrap justify-center items-center gap-2 w-full">
+          <button className="tile">
+            <img
+              src="/icons/add.png"
+              className="rotate-45"
+              alt="go full screen icon"
+              width={20}
+              height={20}
+            />
+            none
+          </button>
           {chronicConditions.slice(0, 10).map((condition, idx) => {
             return (
-              <>
-                <button className="tile" key={idx}>
+              <div key={idx}>
+                <div className="tile">
                   <img
+                    onClick={(event) =>
+                      handleModalclick(
+                        event,
+                        condition?.type,
+                        condition?.description
+                      )
+                    }
                     src="/icons/info.png"
                     alt="information icon"
                     width={20}
                     height={20}
                   />
-                  {condition}
-                </button>
-              </>
+                  {condition?.type}
+                </div>
+              </div>
             );
           })}
           <button className="tile">
