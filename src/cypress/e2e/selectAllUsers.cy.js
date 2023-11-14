d2cded7846886b4caddf11749a2fa2e7a23e11a6
Harry <68599167+harrykeeran12@users.noreply.github.com>
2023-11-14 17:27:36 +0000

    test: set up the select all users test.

---
+++ src/cypress/e2e/selectAllUsers.cy.js
@@ -0,0 +1,15 @@
+/* This API test should print out all the users. */
+describe("This should make a request to the selectAllUsers endpoint. ", () => {
+  it("This should select all users.", () => {
+    let endpoint = Cypress.config("baseUrl") + "/api/selectAllUsers";
+    cy.request({
+      url: endpoint,
+      method: "GET",
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      let exampleOutput = JSON.stringify(response.body[0]);
+    
+      cy.log(exampleOutput);
+    });
+  });
+});
