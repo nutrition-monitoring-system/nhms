3f3644bc9fb27aed2294270639228794fcef199b
Jam <jammoben@gmail.com>
2023-11-30 22:52:30 +0000

    fixed a cypress issue

--- src/cypress/e2e/spec.cy.js
+++ src/cypress/e2e/spec.cy.js
@@ -1,16 +1,16 @@
 // This describes a test suite for checking new user registration and login
 describe("Testing if a new user can log into the database.", () => {
-  const url = "nhms.onrender.com";// sets the base URL for the tests
-// Test case for navigating to the base URL and scrolling the view
+  const url = "nhms.onrender.com"; // sets the base URL for the tests
+  // Test case for navigating to the base URL and scrolling the view
   it("passes", () => {
-    cy.visit(url + "");// Visits the base URL
-    cy.scrollTo("center");// Scrolls to the center of the page
-    cy.scrollTo("bottom", { duration: 1000 });// Scrolls to the bottom over 1 second
+    cy.visit(url + ""); // Visits the base URL
+    cy.scrollTo("center"); // Scrolls to the center of the page
+    cy.scrollTo("bottom", { duration: 1000 }); // Scrolls to the bottom over 1 second
   });
 
   // Test case for registering a new account
   it("registers a new account.", () => {
-    cy.visit(url + "/register");// Visits the registration page
+    cy.visit(url + "/register"); // Visits the registration page
     // Fills out the registration form with new user details
     cy.get('input[name="firstName"]').type("John");
     cy.get('input[name="lastName"]').type("John");
@@ -71,7 +71,7 @@ describe("Testing if a new user can log into the database.", () => {
         cy.get(".Settings").eq(4).invoke("addClass", "bg-secondary");
         cy.get(".Settings").eq(2).invoke("addClass", "bg-secondary");
       });
-          // Fills in breakfast food details
+    // Fills in breakfast food details
     cy.get("#addBreakFast")
       .click()
       .then(() => {
@@ -79,7 +79,7 @@ describe("Testing if a new user can log into the database.", () => {
         cy.get('input[name="Fooddescription"]').type(
           "Milk is a white liquid produced by cows, goats, and sheep"
         );
-         // Finalizes registration and redirects to login page
+        // Finalizes registration and redirects to login page
         cy.get('input[name="Fooddewater"]').type("water");
         cy.get("#addNext").click();
       });
@@ -90,20 +90,20 @@ describe("Testing if a new user can log into the database.", () => {
       });
   });
   // Test case for user login and interaction with the home page
- it('home',() =>{
-  cy.visit(url + "/login");
-  // Logs in with the registered user credentials
-  cy.get('input[name="email"]').type("testaccount@gmail.com");
-  cy.get('input[name="password"]').type("password12345");
-  cy.get('#handleLogin').click();
-  cy.wait(2000); 
-  cy.visit(url + "/home");
+  it("home", () => {
+    cy.visit(url + "/login");
+    // Logs in with the registered user credentials
+    cy.get('input[name="email"]').type("testaccount@gmail.com");
+    cy.get('input[name="password"]').type("password12345");
+    cy.get("#handleLogin").click();
+    cy.wait(3000);
+    cy.visit(url + "/home");
 
-   // Interactions with the home page, searching and navigating tabs
-  cy.get('#search').click();
-  // User logout process
-  cy.get('#usercontent').click();
-  cy.get('#Logout').click();
-  cy.url().should("include", '');
-})
+    // Interactions with the home page, searching and navigating tabs
+    cy.get("#search").click();
+    // User logout process
+    cy.get("#usercontent").click();
+    cy.get("#Logout").click();
+    cy.url().should("include", "");
+  });
 });
