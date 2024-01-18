33fcc6277023622673c5737048045d9864618e0d
Favour Jam <jammoben@gmail.com>
2024-01-18 10:46:54 +0000

    Added authentication to user page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -6,6 +6,7 @@ import { SessionProvider } from "next-auth/react";
 import { useSession, signOut } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Recipes from "../../components/Recipes.jsx";
+import Loading from "../../components/Loading";
 
 export default function Page() {
   return (
@@ -35,19 +36,7 @@ function Home() {
   };
 
   if (status === "loading") {
-    return (
-      <div className="bg-none flex dark:bg-black flex-col gap-7 justify-center items-center absolute inset-0">
-        <img
-          src="/icons/loading.png"
-          width={100}
-          height={100}
-          className="animate-spin "
-        />
-        <div className="text-center font-bold text-[2rem] dark:text-white">
-          Loading...
-        </div>
-      </div>
-    );
+    return <Loading />;
   }
   return (
     <div className="h-screen bg-white flex flex-col min-h-fit">
--- src/app/user/[id]/page.js
+++ src/app/user/[id]/page.js
@@ -1,12 +1,36 @@
 //import Image from 'next/image'
 "use client";
-import UserAvatar from "../../../components/User.jsx";
+import User from "../../../components/User.jsx";
 import Footer from "../../../components/Footer.jsx";
-export default function Home({ params }) {
+import Loading from "../../../components/Loading.jsx";
+import { SessionProvider } from "next-auth/react";
+import { useSession, signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+function Home({ params }) {
+  const router = useRouter();
+  const { session, status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      // The user is not authenticated, handle it here.
+      return router.push("/login");
+    },
+  });
+  if ("loading" === status) {
+    return <Loading />;
+  }
+
   return (
     <>
-      <UserAvatar />
+      <User handsignOut={() => signOut({ callbackUrl: "/" })} />
       <Footer />
     </>
+  );
+}
+
+export default function Page() {
+  return (
+    <SessionProvider>
+      <Home></Home>
+    </SessionProvider>
   );
 }
