95e7778f9143222dece5e742263081301617e3f0
Jam <jammoben@gmail.com>
2024-02-14 15:10:46 +0000

    fix: ui change to the information component

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -21,7 +21,6 @@ export default function Information() {
           scrub: true,
           markers: false,
         },
-        x: -20,
         transformOrigin: "bottom",
         opacity: 0.1,
         stagger: 0.1,
@@ -33,50 +32,36 @@ export default function Information() {
 
   return (
     <>
-      <div5
-        className="relative min-h-fit text-black grid place-content-center"
+      <div
+        className="relative min-h-fit text-black grid place-content-center font-opensans"
         id="information"
       >
         <section
           className="h-screen bg-white text-center p-10 grid place-content-center font-sans
         "
         >
-          <p className="text-lg">
-            Welcome to the future of nutrition management!
+          <p className="text-xl">
+            welcome to the future of nutrition management!
           </p>
           <p className="font-bold">Scroll down</p>
         </section>
-        <section className="h-screen bg-white text-center font-black p-10 grid place-content-center">
+        <section className="h-screen bg-black text-white text-center font-black p-10 grid place-content-center">
           <p className="reveal-type">
-            Our web application is your key to a healthier, more informed
-            lifestyle
+            our application is your key to a healthier, more informed lifestyle.
           </p>
         </section>
-        <section className="h-screen bg-black text-white  text-center font-black p-10 grid place-content-center">
-          <p className="reveal-type">
-            Dive into a world of personalized nutrition with smart insights,
-            tailored recommendations, and easy tracking
-          </p>
+        <section className="h-screen bg-yellow-400 text-black  text-center font-black p-10 grid place-content-center">
+          <p className="reveal-type">discover a dynamic library of recipes.</p>
         </section>
-        <section className="h-screen bg-white text-center font-black p-10 grid place-content-center">
-          <p className="reveal-type">
-            Whether you{"'"}re exploring new dietary preferences, managing
-            chronic conditions, or simply looking to elevate your well-being, we
-            {"'"}ve got you covered.
-          </p>
-        </section>
-        <section className="h-screen bg-black text-white  text-center font-black p-10 grid place-content-center">
-          <p className="reveal-type">Discover a dynamic library of recipes</p>
-        </section>
-        <section className="h-screen bg-white text-center font-black p-10 grid place-content-center">
-          <p className="reveal-type">Set and track your unique health goals</p>
+        <section className="h-screen bg-black text-white text-center font-black p-10 grid place-content-center">
+          <p className="reveal-type">set and track your unique health goals</p>
         </section>
         <section className="h-screen bg-white text-center font-black p-10 grid place-content-center">
           <p className="reveal-type">
-            Start your journey towards a healthier you today!
+            start your journey towards a healthier you today!
           </p>
         </section>
-      </div5>
+      </div>
     </>
   );
 }
