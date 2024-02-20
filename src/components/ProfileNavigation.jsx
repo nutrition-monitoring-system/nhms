238efe9068e4f0655beedb627d2ee763e98be99d
Jam <jammoben@gmail.com>
2024-02-20 14:14:19 +0000

    fix: Fixed some test and some errors on the /admin page

--- src/components/ProfileNavigation.jsx
+++ src/components/ProfileNavigation.jsx
@@ -1,18 +1,18 @@
 "use client";
-import { useRef } from "react";
+import { useEffect, useRef, useState, useMemo } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
 import useSWR from "swr";
 
-
-function HomeUserData() {
+export default function ProfileNavigation() {
+  const menuItems = useRef(null);
+  const [name, setName] = useState("");
+  const [gender, setGender] = useState("M");
   const { data: session, status } = useSession();
 
-  // console.log(`User ID = ${session.user.name}`);
-
-  const sendID = { id: session.user.name };
+  const sendID = { id: session?.user?.name };
   /* Sends the ID of the user to backend. */
 
   const fetcher = (...args) =>
@@ -23,115 +23,105 @@ function HomeUserData() {
         headers: {
           Accept: "application/json",
           "Content-Type": "application/json",
-        
         },
         body: JSON.stringify(sendID),
       },
-      {revalidateIfStale: false}
-      
+      { revalidateIfStale: false }
     ).then((res) => res.json());
 
-  const { data, error } = useSWR("/api/userById", fetcher);
-  /* Uses the SWR Next hook.  */
-
-  if (error || !data) {
-    return null;
-  }
-  console.log(data);
-
-  return data;
-}
-
-
-export default function ProfileNavigation() {
-  const menuItems = useRef(null);
-
+  const { data, error, isLoading } = useSWR("/api/userById", fetcher);
   const handleMenuclick = () => {
     // This function handles the animation for the userMenu. It will animate base on the custom class added
     menuItems.current.classList.toggle("slide-down");
   };
 
-  const { data: session, status } = useSession();
-
-  let name = " ";
-  let gender = " ";
-
-
-
-  if (status == "authenticated"){
-    let data = HomeUserData()
-
-    
-    if (data) {
-      name = data.name + " " + data.surname;
-      gender = data.gender;
+  const userData = useMemo(() => {
+    if (status === "authenticated" && data) {
+      return {
+        name: data.name + " " + data.surname,
+        gender: data.gender,
+      };
     }
+    return {
+      name: "John Smith",
+      gender: "M",
+    };
+  }, [status, data]);
 
-    return (
-      <>
-        <div
-          onClick={handleMenuclick}
-          id="usercontent"
-          className="tile shadow-none hover:shadow-none relative z-10 flex justify-around items-center gap-3 select-none
+  useEffect(() => {
+    setName(userData.name);
+    setGender(userData.gender);
+  }, [userData]);
+
+  /* Uses the SWR Next hook.  */
+
+  return (
+    <>
+      <div
+        onClick={handleMenuclick}
+        id="usercontent"
+        className="tile shadow-none hover:shadow-none relative z-10 flex justify-around items-center gap-3 select-none
     before:absolute before:top-[95%] before:h-1 before:w-full before:translate-x-[-100%] before:rounded-md before:hover:bg-gray-200 before:hover:translate-x-0 before:transition-all before:duration-300"
-        >
-          <Image
-            src={gender === "F" ? "/icons/woman.png" : "/icons/man.png"}
-            width={25}
-            height={25}
-            alt="Person icon"
-            className="ml-2 rounded-[50px]"
-          />
-          <span className="shadow-none text-lg">{name}</span>
-          <div
-            ref={menuItems}
-            className="absolute top-[-6rem] opacity-0 left-0 right-0 rounded-md
+      >
+        <Image
+          src={gender === "F" ? "/icons/woman.png" : "/icons/man.png"}
+          width={25}
+          height={25}
+          alt="Person icon"
+          className="ml-2 rounded-[50px]"
+        />
+        <span className="shadow-none text-lg">
+          {isLoading ? "Loading.." : name}
+        </span>
+        <div
+          ref={menuItems}
+          className="absolute top-[-6rem] opacity-0 left-0 right-0 rounded-md
        shadow-2xl p-2 grid grid-rows-3 gap-1 z-[-10] translate-y-[-100] pointer-events-none"
+        >
+          <div className="tile grid grid-cols-4">
+            <Image
+              src="/icons/account.png"
+              alt="Settings icon"
+              width={20}
+              height={20}
+            />
+            <Link href={"/user"} id="profile">
+              Profile
+            </Link>
+          </div>
+          <div className="tile grid grid-cols-4">
+            <Image
+              src="/icons/settings.png"
+              alt="Settings icon"
+              width={20}
+              height={20}
+            />
+            <Link href={"/user"}>Settings</Link>
+          </div>
+          <div className="tile grid grid-cols-4">
+            <Image
+              src="/icons/translate.png"
+              alt="Language/translate icon"
+              width={20}
+              height={20}
+            />
+            <span>Languages</span>
+          </div>
+          <div
+            id="Logout"
+            className="tile grid grid-cols-4"
+            onClick={() => signOut({ callbackUrl: "/" })}
           >
-            <div className="tile grid grid-cols-4">
-              <Image
-                src="/icons/account.png"
-                alt="Settings icon"
-                width={20}
-                height={20}
-              />
-              <Link href={"/user"}>Profile</Link>
-            </div>
-            <div className="tile grid grid-cols-4">
-              <Image
-                src="/icons/settings.png"
-                alt="Settings icon"
-                width={20}
-                height={20}
-              />
-              <Link href={"/user"}>Settings</Link>
-            </div>
-            <div className="tile grid grid-cols-4">
-              <Image
-                src="/icons/translate.png"
-                alt="Language/translate icon"
-                width={20}
-                height={20}
-              />
-              <span>Languages</span>
-            </div>
-            <div
-              id="Logout"
-              className="tile grid grid-cols-4"
-              onClick={() => signOut({ callbackUrl: "/" })}
-            >
-              <Image
-                src="/icons/logout.png"
-                alt="Logout icon"
-                width={20}
-                height={20}
-              />
-              <span>Logout</span>
-            </div>
+            <Image
+              src="/icons/logout.png"
+              alt="Logout icon"
+              width={20}
+              height={20}
+            />
+            <span>Logout</span>
           </div>
         </div>
-      </>
-    );
-  }
-  
+      </div>
+    </>
+  );
 }
