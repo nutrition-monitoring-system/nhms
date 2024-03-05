75b1762e4ca8c9735ebc7a3ba198dbbd5d3fa797
Harry <68599167+harrykeeran12@users.noreply.github.com>
2024-03-05 14:28:47 +0000

    fix: added a check for a blank symptom.

--- src/pages/api/addSymptom.js
+++ src/pages/api/addSymptom.js
@@ -16,13 +16,15 @@ export default async function handler(req, res) {
         where: { Condition_Type: conditionName },
       });
 
+      const checkSymptom = await prisma.symptoms.findFirst({
+        where: { symptom_name: symptomName },
+      });
+
       /* The condition must be true to add a new symptom. */
       if (checkCondition != null || checkCondition != undefined) {
         console.log(`Condition ${conditionName} is in database.`);
         console.log(`Adding new symptom ${symptomName}.`);
-        const checkSymptom = await prisma.symptoms.findFirst({
-          where: { symptom_name: symptomName },
-        });
+
         if (checkSymptom == null) {
           const newSymptom = await prisma.symptoms
             .create({
@@ -51,6 +53,20 @@ export default async function handler(req, res) {
         console.log("Symptom already in database.");
         return res.status(202).json({ ok: "true" });
       } else {
+        /* Create a symptom if the condition is not present. */
+        if (checkSymptom == null) {
+          const newSymptom = await prisma.symptoms
+            .create({
+              data: {
+                symptom_name: symptomName,
+                symptom_id: newUUID,
+              },
+            })
+            .then(() => {
+              console.log("Symptom added to database.");
+              return res.status(201).json({ ok: "true" });
+            });
+        }
         res.status(404).json({ error: "Could not find condition." });
       }
     }
