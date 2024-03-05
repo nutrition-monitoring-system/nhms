a9c32ed3fb64986912a7b3333f092f4090566fa1
Harry <68599167+harrykeeran12@users.noreply.github.com>
2024-03-05 12:04:48 +0000

    fix: made usability changes.

--- src/app/symptoms/page.js
+++ src/app/symptoms/page.js
@@ -1,50 +1,59 @@
-'use client'
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/navigation'
-import TomSelect from 'tom-select'
-import 'tom-select/dist/css/tom-select.bootstrap5.css'
-import './index.css'
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import TomSelect from "tom-select";
+import "tom-select/dist/css/tom-select.bootstrap5.css";
+import "./index.css";
 
 export default function Symptoms() {
-  const [selected, setSelected] = useState([])
-  const router = useRouter()
+  const [selected, setSelected] = useState([]);
+  const router = useRouter();
 
   const options = [
-    { value: 'Bloating', text: 'Bloating' },
-    { value: 'Diarrhoea', text: 'Diarrhoea' },
-    { value: 'Stomach Pain', text: 'Stomach Pain' },
-    { value: 'Other', text: 'Other' },
-  ]
+    { value: "Bloating", text: "Bloating" },
+    { value: "Diarrhoea", text: "Diarrhoea" },
+    { value: "Stomach Pain", text: "Stomach Pain" },
+    { value: "Other", text: "Other" },
+  ];
 
-  const handleSelect = value => {
-    setSelected(value)
-  }
+  const handleSelect = (value) => {
+    setSelected(value);
+  };
 
   useEffect(() => {
-    const inst = new TomSelect('#select-state', {
+    const inst = new TomSelect("#select-state", {
       options,
-      plugins: ['remove_button', 'clear_button'],
+      plugins: ["remove_button", "clear_button"],
       onChange: handleSelect,
-    })
-    return () => inst.destroy()
-  }, [])
-
+      create: true, createOnBlur: true,
+      render: {
+        item: function(data, escape) {
+          return '<div class=" !px-3 !bg-primarylight !rounded-lg !hover:bg-primarylight/75">' + escape(data.text) + '</div>';
+        },
+      }
+    });
+    return () => inst.destroy();
+  }, []);
 
   const handleSubmit = () => {
-    if(selected.length === 0) return
-    console.log(selected)
-    const params = new URLSearchParams()
-    params.append('selected', selected.join(','))
-    router.push(`/symptoms/slide?${params.toString()}`)
-  }
+    if (selected.length === 0) return;
+    console.log(selected);
+    const params = new URLSearchParams();
+    params.append("selected", selected.join(","));
+    router.push(`/symptoms/slide?${params.toString()}`);
+  };
 
   return (
     <>
-      <div className="flex justify-center items-center flex-col gap-4 py-4">
+      <div className="flex justify-center items-center flex-col gap-10 py-4 ">
         <h1 className="text-4xl font-bold">Symptoms:</h1>
-
         <div className="w-1/2 min-h-[40vh] flex items-center flex-col gap-6">
-          <input type="date" className="w-full p-2 rounded-lg" style={{ backgroundColor: 'rgb(220,150,150)' }} />
+          <input
+            type="date"
+            className="appearance-none w-full p-3 rounded-lg !bg-primary "
+            placeholder="Enter a date: "
+            required
+          />
           {/* <input
             type="search"
             placeholder="Type to search symptoms"
@@ -55,9 +64,9 @@ export default function Symptoms() {
             id="select-state"
             name="state[]"
             multiple
-            placeholder="Type to search symptoms"
-            autocomplete="off"
-            className="w-full form-control"
+            placeholder="Type to search symptoms:"
+            autoComplete="off"
+            className="w-full form-control bg-primary"
             // onChange={handleSelect}
           />
         </div>
@@ -73,5 +82,5 @@ export default function Symptoms() {
         </div>
       </div>
     </>
-  )
+  );
 }
