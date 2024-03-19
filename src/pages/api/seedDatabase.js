359d38af755c2d41c9c1fb124f4226118d369eff
Harry <68599167+harrykeeran12@users.noreply.github.com>
2024-03-19 13:53:13 +0000

    fix: added some recipes to the database.

--- src/pages/api/seedDatabase.js
+++ src/pages/api/seedDatabase.js
@@ -3,6 +3,7 @@ import { v1 } from "uuid";
 import {
   AllergiesInformation,
   chronicConditions,
+  pinnedRecipes,
 } from "@/utils/dataRegistration.js";
 export default async function handler(req, res) {
   if (req.method == "POST") {
@@ -47,7 +48,41 @@ export default async function handler(req, res) {
             }
           });
       });
+      console.log("Adding Dr Gostic's recipes to database.");
 
+      pinnedRecipes.forEach(async (recipeEntry) => {
+        const checkRecipe = await prisma.recipe
+          .findFirst({
+            where: {
+              recipeName: recipeEntry.name,
+            },
+          })
+          .then(async (response) => {
+            // console.log(response)
+            if (response == null) {
+              /* Add the new data into the database. */
+              let addNewRecipe = await prisma.recipe.create({
+                data: {
+                  recipeID: v1().slice(0, 32),
+                  recipeName: recipeEntry.name,
+                  serving: recipeEntry.servings,
+                  recipeType: recipeEntry.type,
+                  recipeIngredients: recipeEntry.ingredients.join(","),
+                  recipeInstructions: recipeEntry.directions ? recipeEntry.directions : "",
+                  cookTime: recipeEntry.cooking_time,
+                  prepTime: recipeEntry.prepTime ? recipeEntry.prepTime : "0 minutes",
+                  freezeTime: recipeEntry.freezeTime
+                    ? recipeEntry.freezeTime
+                    : "0 minutes",
+                  calories: recipeEntry.nutrition["calories"],
+                  carbohydrates: recipeEntry.nutrition["carbohydrates"],
+                  fat: recipeEntry.nutrition["fat"],
+                  protein: recipeEntry.nutrition["protein"],
+                },
+              });
+            }
+          });
+      });
       console.log("Database populated.");
       return res.status(200).json({ ok: true });
     } catch (error) {
