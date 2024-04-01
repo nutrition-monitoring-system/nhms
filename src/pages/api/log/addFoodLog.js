38421eb0473c4e91931347f1d8c0976bc499b096
Harry <68599167+harrykeeran12@users.noreply.github.com>
2024-04-01 17:27:07 +0100

    fix: wrote new api route for water log + added a test.

--- src/pages/api/log/addFoodLog.js
+++ src/pages/api/log/addFoodLog.js
@@ -1,4 +1,4 @@
-/* This method needs to create a new food log. This is done by first creating a new log item, with a unique log ID, the userID, the timestamp, which defaults to now if not put in, and setting the log type as food. */
+/* This method needs to create a new food log. This is done by first creating a new log item, with a unique log ID, the userID, the timestamp(MUST BE IN AN ISO FORMAT), which defaults to now if not put in, and setting the log type as food. */
 
 /* After this logID is created, then a new foodLog will be created, that links the food item, and the log.*/
 import { v1 } from "uuid";
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
   if (req.method == "POST") {
     // Process a POST request
     if (req.body) {
-      let { userID, timestamp, food} = req.body;
+      let { userID, timestamp, food } = req.body;
       /* Check if userID is in the database already. */
       const checkUser = await prisma.user.findFirst({
         where: { userID: userID },
@@ -21,17 +21,36 @@ export default async function handler(req, res) {
       }
 
       if (timestamp == null) {
-        timestamp = new Date.now()
-        console.log(`${timestamp} is set to now.`)
+        timestamp = new Date().toISOString();
+        console.log(`${timestamp} is set to now.`);
       }
-
-
 
       let newLogID = v1().slice(0, 32);
 
       console.log(newLogID);
 
-      return res.status(200).send("API on.");
+      const newLog = await prisma.log
+        .create({
+          data: {
+            logID: newLogID,
+            logType: "food",
+            timestamp: timestamp,
+            user_UserID: userID,
+          },
+        })
+        .then(async (response) => {
+          if((response) != null){
+            let newFoodEntry = await prisma.foodLog.create({
+              data: {
+                food_foodID: v1().slice(0, 32),
+                log_logID: newLogID,
+                foodQuantity: food.foodQuantity
+              }
+            })
+          }
+        });
+
+      return res.status(200).json({ ok: true });
     } else {
       res.status(404).json({ error: "Nothing present in body." });
     }
