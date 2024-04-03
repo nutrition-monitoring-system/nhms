27bc67f0f34df8b204830804d9d4a259e62541d3
Harry <68599167+harrykeeran12@users.noreply.github.com>
2024-04-04 00:10:42 +0100

    bug: multiple foodlog items cannot be assigned to the same food.

--- src/pages/api/log/addFoodLog.js
+++ src/pages/api/log/addFoodLog.js
@@ -49,41 +49,55 @@ export default async function handler(req, res) {
           })
           .then(async (response) => {
             /* Check if food exists or not. */
-            let newFoodID = v1().slice(0, 32);
-            try {
-              let newFood = await prisma.food.create({
-                data: {
-                  foodID: newFoodID,
-                  foodName: food.foodName,
-                  isDrink: false,
-                  sodium: food.sodium,
-                  protein: food.protein,
-                  fat: food.fat,
-                  vitamins: food.vitamins,
-                  calcium: food.calcium,
-                  iron: food.iron,
-                  carbohydrates: food.carbohydrates,
-                  potassium: food.potassium,
-                  fibre: food.fibre,
-                  sugar: food.sugar,
-                },
-              });
-              console.log(
-                `New food added with name ${food.foodName}, and ID ${newFoodID}`
-              );
-            } catch {
-              return res
-                .status(500)
-                .json({ error: "Food not in correct format. Food must be an Object and have the name and the quantity." });
-            }
             if (response != null) {
-              let newFoodLogEntry = await prisma.foodLog.create({
-                data: {
-                  food_foodID: newFoodID,
-                  log_logID: newLogID,
-                  foodQuantity: food.foodQuantity,
-                },
-              });
+              let newFoodID = v1().slice(0, 32);
+              try {
+                /* Check if food name already exists. */
+                let checkFood = await prisma.food.findFirstOrThrow({
+                  where: {
+                    foodName: food.foodName,
+                  },
+                });
+                newFoodID = checkFood.foodID;
+              } catch (error) {
+                try {
+                  let newFood = await prisma.food.create({
+                    data: {
+                      foodID: newFoodID,
+                      foodName: food.foodName,
+                      isDrink: false,
+                      sodium: food.sodium,
+                      protein: food.protein,
+                      fat: food.fat,
+                      vitamins: food.vitamins,
+                      calcium: food.calcium,
+                      iron: food.iron,
+                      carbohydrates: food.carbohydrates,
+                      potassium: food.potassium,
+                      fibre: food.fibre,
+                      sugar: food.sugar,
+                    },
+                  });
+                  console.log(
+                    `New food added with name ${food.foodName}, and ID ${newFoodID}`
+                  );
+                } catch {
+                  return res.status(500).json({
+                    error:
+                      "Food not in correct format. Food must be an Object and have the name and the quantity.",
+                  });
+                }
+              }
+              finally{
+
+                let newFoodLogEntry = await prisma.foodLog.create({
+                  data: {
+                    food_foodID: newFoodID,
+                    log_logID: newLogID,
+                    foodQuantity: food.foodQuantity,
+                  },
+                });
+              }
             }
           });
 
